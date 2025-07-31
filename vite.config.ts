eceab201ce01a41fe4fca035ed3785009810b4dc
randomplayables <randomplayables@proton.me>
2025-07-31 18:29:12 +0000

    feat: Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+      port: Number(env.VITE_PORT) || 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+});
